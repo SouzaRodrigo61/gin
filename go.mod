71568b5a7070cca3e2abcb7547a483ff12b4be77
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-06-05 10:35:21 -0300

    feat: add gorouter for manager paths

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module gin
 
 go 1.14
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/vardius/gorouter v3.0.4+incompatible
+	github.com/vardius/gorouter/v4 v4.5.1
+)
--- main.go
+++ main.go
@@ -1,65 +1,51 @@
 package main
 
 import (
+    "fmt"
 	"log"
-	"net/http"
-	"strings"
+    "time"
+    "net/http"
+
+    "github.com/vardius/gorouter/v4"
+    "github.com/vardius/gorouter/v4/context"
 )
 
-type httpMethod string
-type urlPattern string
+func logger(next http.Handler) http.Handler {
+  fn := func(w http.ResponseWriter, r *http.Request) {
+    t1 := time.Now()
+    next.ServeHTTP(w, r)
+    t2 := time.Now()
+    log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+  }
 
-type routeRules struct {
-	methods map[httpMethod]http.Handler
+  return http.HandlerFunc(fn)
 }
 
-type router struct {
-	routes map[urlPattern]routeRules
+func example(next http.Handler) http.Handler {
+  fn := func(w http.ResponseWriter, r *http.Request) {
+    // do smth
+    next.ServeHTTP(w, r)
+  }
+
+  return http.HandlerFunc(fn)
 }
 
-func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	foundRoute, exists := r.routes[urlPattern(req.URL.Path)]
-	if !exists {
-		http.NotFound(w, req)
-		return
-	}
-	handler, exists := foundRoute.methods[httpMethod(req.Method)]
-	if !exists {
-		notAllowed(w, req, foundRoute)
-		return
-	}
-	handler.ServeHTTP(w, req)
+func index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Welcome!\n")
 }
 
-func (r *router) HandleFunc(method httpMethod, pattern urlPattern, f func(w http.ResponseWriter, req *http.Request)) {
-	rules, exists := r.routes[pattern]
-	if !exists {
-		rules = routeRules{methods: make(map[httpMethod]http.Handler)}
-		r.routes[pattern] = rules
-	}
-	rules.methods[method] = http.HandlerFunc(f)
-}
-
-func notAllowed(w http.ResponseWriter, req *http.Request, r routeRules) {
-	methods := make([]string, 1)
-	for k := range r.methods {
-		methods = append(methods, string(k))
-	}
-	w.Header().Set("Allow", strings.Join(methods, " "))
-	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-}
-
-func New() *router {
-	return &router{routes: make(map[urlPattern]routeRules)}
-}
-
-func handler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("hello"))
+func hello(w http.ResponseWriter, r *http.Request) {
+    params, _ := context.Parameters(r.Context())
+    fmt.Fprintf(w, "hello, %s!\n", params.Value("name"))
 }
 
 func main() {
-	r := New()
-	r.HandleFunc(http.MethodPost, "/test", handler)
-	log.Print("Listening... ")
-	http.ListenAndServe(":8000", r)
+    // apply middleware to all routes
+    // can pass as many as you want
+    router := gorouter.New(logger, example)
+
+    router.GET("/", http.HandlerFunc(index))
+    router.GET("/hello/{name}", http.HandlerFunc(hello))
+
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
