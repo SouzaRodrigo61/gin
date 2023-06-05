909ac7863de282be237951b220260fa0fb6e2719
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-06-05 11:02:33 -0300

    feat: main formated port and get from env

--- main.go
+++ main.go
@@ -1,51 +1,60 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"log"
-    "time"
-    "net/http"
+	"net/http"
+	"os"
+	"time"
 
-    "github.com/vardius/gorouter/v4"
-    "github.com/vardius/gorouter/v4/context"
+	"github.com/joho/godotenv"
+
+	"github.com/vardius/gorouter/v4"
+	"github.com/vardius/gorouter/v4/context"
 )
 
 func logger(next http.Handler) http.Handler {
-  fn := func(w http.ResponseWriter, r *http.Request) {
-    t1 := time.Now()
-    next.ServeHTTP(w, r)
-    t2 := time.Now()
-    log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
-  }
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+	}
 
-  return http.HandlerFunc(fn)
+	return http.HandlerFunc(fn)
 }
 
 func example(next http.Handler) http.Handler {
-  fn := func(w http.ResponseWriter, r *http.Request) {
-    // do smth
-    next.ServeHTTP(w, r)
-  }
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// do smth
+		next.ServeHTTP(w, r)
+	}
 
-  return http.HandlerFunc(fn)
+	return http.HandlerFunc(fn)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Welcome!\n")
+	fmt.Fprint(w, "Welcome!\n")
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    params, _ := context.Parameters(r.Context())
-    fmt.Fprintf(w, "hello, %s!\n", params.Value("name"))
+	params, _ := context.Parameters(r.Context())
+	fmt.Fprintf(w, "hello, %s!\n", params.Value("name"))
 }
 
 func main() {
-    // apply middleware to all routes
-    // can pass as many as you want
-    router := gorouter.New(logger, example)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    router.GET("/", http.HandlerFunc(index))
-    router.GET("/hello/{name}", http.HandlerFunc(hello))
+	// apply middleware to all routes
+	// can pass as many as you want
+	router := gorouter.New(logger, example)
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+	router.GET("/", http.HandlerFunc(index))
+	router.GET("/hello/{name}", http.HandlerFunc(hello))
+	port := os.Getenv("PORT")
+	formatedPort := ":" + port
+	log.Fatal(http.ListenAndServe(formatedPort, router))
 }
